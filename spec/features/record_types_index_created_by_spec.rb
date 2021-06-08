4eaa66b7e513761258b428404d5d7c3802eb1e61
Serge Vinogradoff <zoopyserg@gmail.com>
2021-06-08 20:22:31 +0300

    record types created by

---
+++ spec/features/record_types_index_created_by_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.feature "ConnectionsIndex Created By", type: :feature do
+  let!(:user1) { create :user, :confirmed, :free, first_name: 'John', last_name: 'Smith', username: 'something1', email: 'jacky.daniels@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
+  let!(:user2) { create :user, :confirmed, :free, first_name: 'Bob', last_name: 'Smith', username: 'something2', email: 'jack.daniels@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
+  let!(:public_record_type) { create :record_type, user: user1, is_public: true }
+  let!(:private_record_type) { create :record_type, user: user2, is_public: false }
+
+  context 'not signed in' do
+    before { visit record_types_path }
+
+    it 'should say who created one record' do
+      expect(page).to have_content 'John Smith'
+      click_on 'John Smith'
+      expect(current_path).to eq user_path(user1)
+    end
+
+    it 'should not say names of people who do not have public record_types' do
+      expect(page).to have_no_content 'Bob Smith'
+    end
+  end
+
+  context 'signed in' do
+    before do
+      visit root_path
+      sign_in('jack.daniels@gmail.com', 'rediculouslycomplexpassword54321')
+      visit record_types_path
+    end
+
+    it 'should say who created public record' do
+      expect(page).to have_content 'John Smith'
+      click_on 'John Smith'
+      expect(current_path).to eq user_path(user1)
+    end
+
+    it 'should say I created a private record' do
+      expect(page).to have_content 'Bob Smith'
+      click_on 'Bob Smith'
+      expect(current_path).to eq user_path(user2)
+    end
+  end
+end
