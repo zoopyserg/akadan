add451fa3f375da09858f23358b4e87fa51b2b7e
Serge Vinogradoff <zoopyserg@gmail.com>
2021-07-13 10:18:37 +0300

    displaying record type

--- spec/features/records/index/records_index_showing_record_type_spec.rb
+++ spec/features/records/index/records_index_showing_record_type_spec.rb
@@ -1,36 +1,72 @@
 require 'rails_helper'
 
 RSpec.feature "Record Created By", type: :feature do
-  let!(:user1) { create :user, :confirmed, :free, first_name: 'John', last_name: 'Smith', username: 'something1', email: 'jacky.daniels@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
-  let!(:user2) { create :user, :confirmed, :free, first_name: 'Bob', last_name: 'Smith', username: 'something2', email: 'jack.daniels@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
-  let!(:public_record) { create :record, description: 'Public Description', user: user1, is_public: true }
-  let!(:private_record) { create :record, description: 'Private Description', user: user2, is_public: false }
+  let!(:user1) { create :user, :confirmed, :free, email: 'user1@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
+  let!(:user2) { create :user, :confirmed, :free, email: 'user2@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
+  let!(:record_type1) { create :record_type, name: 'Type 1', user: user1, is_public: true }
+  let!(:record_type2) { create :record_type, name: 'Type 2', user: user1, is_public: false }
+  let!(:record_type3) { create :record_type, name: 'Type 3', user: user2, is_public: true }
+  let!(:record_type4) { create :record_type, name: 'Type 4', user: user2, is_public: false }
 
-  xcontext 'not signed in' do
+  let!(:record) { create :record, record_type: record_type, user: user1, is_public: true }
+
+  context 'not signed in' do
     before { visit records_path }
 
-    it 'should say who created one record' do
-      expect(page).to have_content 'Public Description'
+    context 'public my type' do
+      let(:record_type) { record_type1 }
+
+      it { expect(page).to have_content 'Type:Type 1' }
     end
 
-    it 'should not say names of people who do not have public connections' do
-      expect(page).to have_no_content 'Private Description'
+    context 'private my type' do
+      let(:record_type) { record_type2 }
+
+      it { expect(page).to have_content 'Type:Type 2' }
+    end
+
+    context 'public someone elses type' do
+      let(:record_type) { record_type3 }
+
+      it { expect(page).to have_content 'Type:Type 3' }
+    end
+
+    context 'private someone elses type' do
+      let(:record_type) { record_type4 }
+
+      it { expect(page).to have_content 'Type:Type 4' }
     end
   end
 
-  xcontext 'signed in' do
+  context 'signed in' do
     before do
       visit root_path
-      sign_in('jack.daniels@gmail.com', 'rediculouslycomplexpassword54321')
+      sign_in('user1@gmail.com', 'rediculouslycomplexpassword54321')
       visit records_path
     end
 
-    it 'should say who created public record' do
-      expect(page).to have_content 'Public Description'
+    context 'public my type' do
+      let(:record_type) { record_type1 }
+
+      it { expect(page).to have_content 'Type:Type 1' }
     end
 
-    it 'should say I created a private record' do
-      expect(page).to have_content 'Private Description'
+    context 'private my type' do
+      let(:record_type) { record_type2 }
+
+      it { expect(page).to have_content 'Type:Type 2' }
+    end
+
+    context 'public someone elses type' do
+      let(:record_type) { record_type3 }
+
+      it { expect(page).to have_content 'Type:Type 3' }
+    end
+
+    context 'private someone elses type' do
+      let(:record_type) { record_type4 }
+
+      it { expect(page).to have_content 'Type:Type 4' }
     end
   end
 end
