97247aec08ea2fbb5f5ba9da0b322fe4ef3a4e77
Serge Vinogradoff <zoopyserg@gmail.com>
2021-07-31 17:32:10 +0300

    solved red test in sign in

--- spec/features/auth/signin_spec.rb
+++ spec/features/auth/signin_spec.rb
@@ -55,7 +55,7 @@ RSpec.feature "Signins", type: :feature do
       end
     end
 
-    context 'success' do
+    context 'success', :records_index do
       let!(:user) { create :user, :confirmed, :free, username: 'something',
                                                      email: 'jack.daniels@gmail.com',
                                                      password: 'rediculouslycomplexpassword54321',
