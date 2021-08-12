e4f9c79836ff092960a93a1caaf458aef7fea0e5
Serge Vinogradoff <zoopyserg@gmail.com>
2021-08-12 04:38:31 +0300

    counters of my and other people's records in the tree.

--- spec/features/records/index/record_info/expanded_view/totals/records_index_total_unsolved_records_connected_for_others_spec.rb
+++ spec/features/records/index/record_info/expanded_view/totals/records_index_total_unsolved_records_connected_for_others_spec.rb
@@ -1,47 +1,235 @@
 require 'rails_helper'
 
-RSpec.feature "Record Created By", :records_index, type: :feature do
-  let!(:user1) { create :user, :confirmed, :free, first_name: 'John', last_name: 'Smith', username: 'something1', email: 'jacky.daniels@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
-  let!(:user2) { create :user, :confirmed, :free, first_name: 'Bob', last_name: 'Smith', username: 'something2', email: 'jack.daniels@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
+RSpec.feature "Unsolved Tree Records Counter for others", :records_index, type: :feature do
+  let!(:user1) { create :user, :confirmed, email: 'user1@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
+  let!(:user2) { create :user, :confirmed, email: 'user2@gmail.com', password: 'rediculouslycomplexpassword54321', password_confirmation: 'rediculouslycomplexpassword54321' }
+  let!(:solution_connection_type_id) { ConnectionType.find_or_create_by(name: 'Is Solved By...').id }
 
-  let!(:public_record) { create :record, user: user1, is_public: true }
-
-  let!(:public_record1) { create :record, user: user1, is_public: true }
-  let!(:public_record2) { create :record, user: user1, is_public: true }
-  let!(:public_record3) { create :record, user: user2, is_public: true }
-  let!(:public_record4) { create :record, user: user2, is_public: true }
-  let!(:public_record5) { create :record, user: user2, is_public: true }
-
-  let!(:connection1) { create :connection, record_a: public_record2, record_b: public_record }
-  let!(:connection2) { create :connection, record_a: public_record1, record_b: public_record2 }
-  let!(:connection3) { create :connection, record_a: public_record2, record_b: public_record3 }
-  let!(:connection4) { create :connection, record_a: public_record4, record_b: public_record }
-  let!(:connection5) { create :connection, record_a: public_record4, record_b: public_record5 }
-
-  let!(:private_record6) { create :record, user: user2, is_public: false }
-  let!(:private_record7) { create :record, user: user2, is_public: false }
-
-  let!(:connection6) { create :connection, record_a: public_record5, record_b: private_record6 }
-  let!(:connection7) { create :connection, record_a: public_record3, record_b: private_record7 }
-
-  let!(:unconnected_public_record1) { create :record, user: user1, is_public: true }
-  let!(:unconnected_public_record2) { create :record, user: user2, is_public: true }
-
-  # not sure if I should or should not know that someone has a private record connected to my record
   context 'not signed in' do
+    let!(:public_record) { create :record, user: user1, is_public: true }
+
     before { visit records_path }
 
-    it { expect(page).to have_no_content 'Other people\'s records connected:' }
+    it { expect(page).to have_no_content "Other people's unsolved records in tree" }
   end
 
-  context 'signed in' do
+  context 'signed in as user2 (not owner)' do
     before do
       visit root_path
-      sign_in('jack.daniels@gmail.com', 'rediculouslycomplexpassword54321')
+      sign_in('user2@gmail.com', 'rediculouslycomplexpassword54321')
       visit records_path
     end
 
-    it { expect(page).to have_content 'Other people\'s records connected: 2' }
-    it { expect(page).to have_no_content 'Other people\'s records connected: 7' }
+    context 'single unsolved record' do
+      let!(:public_record) { create :record, user: user1, is_public: true }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 1" }
+    end
+
+    context 'single solved record' do
+      let!(:public_record) { create :record, user: user1, is_public: true }
+      let!(:public_solution) { create :record, user: user1, is_public: true }
+
+      let!(:solution_connection) { create :connection, record_a: public_record, record_b: public_solution, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 1" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two destructive subrecords, one solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: true }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 3" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two destructive subrecords, both solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+      let!(:public_record5) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: true }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection1) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+      let!(:solution_connection2) { create :connection, record_a: public_record2, record_b: public_record5, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 2" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two non-destructive subrecords, both solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+      let!(:public_record5) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: false }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection1) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+      let!(:solution_connection2) { create :connection, record_a: public_record2, record_b: public_record5, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 3" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two subrecords, one destructive one none-destructive, both solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+      let!(:public_record5) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type1) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: true }
+      let!(:subsystem_connection_type2) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: false }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type1, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type2, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection1) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+      let!(:solution_connection2) { create :connection, record_a: public_record2, record_b: public_record5, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 3" } # because in the tree the solution is itself "unsolved"
+    end
+  end
+
+  context 'signed in as user1 (owner)' do
+    before do
+      visit root_path
+      sign_in('user1@gmail.com', 'rediculouslycomplexpassword54321')
+      visit records_path
+    end
+
+    context 'single unsolved record' do
+      let!(:public_record) { create :record, user: user1, is_public: true }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 0" }
+    end
+
+    context 'single solved record' do
+      let!(:public_record) { create :record, user: user1, is_public: true }
+      let!(:public_solution) { create :record, user: user1, is_public: true }
+
+      let!(:solution_connection) { create :connection, record_a: public_record, record_b: public_solution, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 0" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two destructive subrecords, one solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: true }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 0" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two destructive subrecords, both solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+      let!(:public_record5) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: true }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection1) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+      let!(:solution_connection2) { create :connection, record_a: public_record2, record_b: public_record5, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 0" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two non-destructive subrecords, both solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+      let!(:public_record5) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: false }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection1) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+      let!(:solution_connection2) { create :connection, record_a: public_record2, record_b: public_record5, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 0" } # because in the tree the solution is itself "unsolved"
+    end
+
+    context 'two subrecords, one destructive one none-destructive, both solved' do
+      let!(:public_record1) { create :record, user: user1, is_public: true }
+      let!(:public_record2) { create :record, user: user1, is_public: true }
+      let!(:public_record3) { create :record, user: user1, is_public: true }
+
+      let!(:public_record4) { create :record, user: user1, is_public: true }
+      let!(:public_record5) { create :record, user: user1, is_public: true }
+
+      let!(:subsystem_connection_type1) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: true }
+      let!(:subsystem_connection_type2) { create :connection_type, user: user1, is_public: true, name: 'subrecord', destructive: false }
+
+      let!(:connection1) { create :connection, connection_type: subsystem_connection_type1, record_a: public_record1, record_b: public_record2 }
+      let!(:connection2) { create :connection, connection_type: subsystem_connection_type2, record_a: public_record1, record_b: public_record3 }
+
+      let!(:solution_connection1) { create :connection, record_a: public_record3, record_b: public_record4, connection_type_id: solution_connection_type_id }
+      let!(:solution_connection2) { create :connection, record_a: public_record2, record_b: public_record5, connection_type_id: solution_connection_type_id }
+
+      before { visit records_path }
+
+      it { expect(page).to have_content "Other people's unsolved records in tree: 0" } # because in the tree the solution is itself "unsolved"
+    end
   end
 end
