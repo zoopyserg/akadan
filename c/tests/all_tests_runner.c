298f7998443c91e2a90890d2213b3018ae6723d2
Serge Vinogradoff <zoopyserg@gmail.com>
2024-03-03 22:30:56 +0200

    test rank case 2

--- c/tests/all_tests_runner.c
+++ c/tests/all_tests_runner.c
@@ -13,6 +13,7 @@
 #include "all_solved_tree_record_ids_two_non_destructive_subrecords_both_solved_test.h"
 #include "all_solved_tree_record_ids_one_destructive_one_non_destructive_subrecord_both_solved_test.h"
 #include "all_solved_tree_record_ids_rank_case_one_test.h"
+#include "all_solved_tree_record_ids_rank_case_two_test.h"
 #include "common_insert.h"
 #include "common_cleanup.h"
 
@@ -42,7 +43,8 @@ int main() {
         // (NULL == CU_add_test(pSuite, "Test All Solved Tree Record IDs Two Destructive Subrecords Both Solved", test_all_solved_tree_record_ids_two_destructive_subrecords_both_solved)) ||
         // (NULL == CU_add_test(pSuite, "Test All Solved Tree Record IDs Two Non Destructive Subrecords Both Solved", test_all_solved_tree_record_ids_two_non_destructive_subrecords_both_solved)) ||
         // (NULL == CU_add_test(pSuite, "Test All Solved Tree Record IDs One Destructive One Non Destructive Subrecord Both Solved", test_all_solved_tree_record_ids_one_destructive_one_non_destructive_subrecord_both_solved)) ||
-        (NULL == CU_add_test(pSuite, "Test All Solved Tree Record IDs Rank Case One", test_all_solved_tree_record_ids_rank_case_one))
+        // (NULL == CU_add_test(pSuite, "Test All Solved Tree Record IDs Rank Case One", test_all_solved_tree_record_ids_rank_case_one)) ||
+        // (NULL == CU_add_test(pSuite, "Test All Solved Tree Record IDs Rank Case Two", test_all_solved_tree_record_ids_rank_case_two)) ||
        ) {
         CU_cleanup_registry();
         return CU_get_error();
